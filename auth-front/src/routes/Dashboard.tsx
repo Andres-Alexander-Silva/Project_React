642214bdd57447b923f54aec211bfc093ef493de
andresalexanderss <andresalexanderss@ufps.edu.co>
2023-09-25 10:59:20 -0500

    Se sube el front realizado en react.js

---
+++ auth-front/src/routes/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { useAuth } from "../auth/AuthProvider";
+import PortalLayout from "../layout/PortalLayout";
+
+export default function Dashboard() {
+    const auth = useAuth();
+    return (
+        <PortalLayout>
+            <h1>Dashboard {auth.getUser()?.name}</h1>
+        </PortalLayout>
+    );
+}
